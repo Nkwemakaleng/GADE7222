18fe176c10722596a5a4050ce5608e3376161231
Nkwemakaleng <nkwemakaleng@gmail.com>
2024-09-23 17:13:21 +0200

    Unreal
    It exists

---
+++ GADE7222_Quake/Intermediate/Build/Win64/x64/GADE7222_QuakeEditor/Development/UnrealEd/SharedPCH.UnrealEd.Project.ValApi.Cpp20.InclOrderOldest.h
@@ -0,0 +1,3 @@
+// PCH for Editor/UnrealEd/Public/UnrealEdSharedPCH.h
+#include "C:/Users/ASUS/Documents/GitHub/GADE7222/GADE7222_Quake/Intermediate/Build/Win64/x64/GADE7222_QuakeEditor/Development/UnrealEd/SharedDefinitions.UnrealEd.Project.ValApi.Cpp20.InclOrderOldest.h"
+#include "Editor/UnrealEd/Public/UnrealEdSharedPCH.h"
---
+++ GADE7222_Quake/Source/GADE7222_Quake/GADE7222_Quake.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "GADE7222_Quake.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, GADE7222_Quake, "GADE7222_Quake" );
